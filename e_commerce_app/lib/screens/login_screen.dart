2c9b3d0fb3759e92d9e16b4edcd27aeb33fe9160
Eslam Elezaby <eslamelezaby98@gmail.com>
2021-09-09 14:18:21 +0200

    adding sign in with email and password

--- e_commerce_app/lib/screens/login_screen.dart
+++ e_commerce_app/lib/screens/login_screen.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginScreen extends GetWidget<Auth> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -14,98 +15,108 @@ class LoginScreen extends GetWidget<Auth> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.only(top: 80, right: 30, left: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Welcome',
-                    style: TextStyle(
-                      // color: Colors.black,
-                      fontSize: 30,
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Text(
-                      'Sign Up',
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Welcome',
                       style: TextStyle(
-                        color: Color(0xff61c669),
-                        fontSize: 16,
+                        // color: Colors.black,
+                        fontSize: 30,
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Sign in to contine',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 13,
+                    InkWell(
+                      onTap: () {},
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          color: Color(0xff61c669),
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(height: 60),
-              CustomTextField(
-                hintText: 'max@example.com',
-                title: 'Email',
-                onSave: (value) {},
-                validator: (value) {},
-              ),
-              SizedBox(height: 30),
-              CustomTextField(
-                hintText: '***********',
-                title: 'Password',
-                isObscured: true,
-                onSave: (value) {},
-                validator: (value) {},
-              ),
-              SizedBox(height: 5),
-              Text(
-                'Forget a password',
-                style: TextStyle(color: Colors.blue),
-              ),
-              SizedBox(height: 30),
-              CustomButton(
-                title: 'Sign In',
-                buttonSize: size.width,
-                onPress: () {},
-              ),
-              SizedBox(height: 30),
-              Center(
-                child: Text(
-                  '-OR-',
+                SizedBox(height: 10),
+                Text(
+                  'Sign in to contine',
                   style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
+                    fontSize: 13,
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              CustomSocialButton(
-                title: 'Continue With Google',
-                buttonColor: Colors.white,
-                imagePath: 'assets/images/google_logo.png',
-                onTap: () async {
-                  await controller.signInWithGoogle();
-                  Get.to(HomeScreen());
-                  // Get.to(HomeScreen());
-                },
-              ),
-              SizedBox(height: 15),
-              CustomSocialButton(
-                title: 'Continue With Facebook',
-                buttonColor: Colors.white,
-                imagePath: 'assets/images/facebook_logo.png',
-                onTap: () async{
-                  await controller.signInWithFacebook();
-                  Get.to(() => HomeScreen());
-                },
-              ),
-            ],
+                SizedBox(height: 60),
+                CustomTextField(
+                  hintText: 'max@example.com',
+                  title: 'Email',
+                  onSave: (value) {
+                    controller.email = value;
+                  },
+                  validator: (value) {},
+                ),
+                SizedBox(height: 30),
+                CustomTextField(
+                  hintText: '***********',
+                  title: 'Password',
+                  isObscured: false,
+                  onSave: (value) {
+                    controller.password = value;
+                  },
+                  validator: (value) {},
+                ),
+                SizedBox(height: 5),
+                Text(
+                  'Forget a password',
+                  style: TextStyle(color: Colors.blue),
+                ),
+                SizedBox(height: 30),
+                CustomButton(
+                  title: 'Sign In',
+                  buttonSize: size.width,
+                  onPress: () {
+                    _formKey.currentState.save();
+                    controller.signInWithEmailAndPassword();
+                  },
+                ),
+                SizedBox(height: 30),
+                Center(
+                  child: Text(
+                    '-OR-',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                CustomSocialButton(
+                  title: 'Continue With Google',
+                  buttonColor: Colors.white,
+                  imagePath: 'assets/images/google_logo.png',
+                  onTap: () async {
+                    await controller.signInWithGoogle();
+                    Get.to(HomeScreen());
+                    // Get.to(HomeScreen());
+                  },
+                ),
+                SizedBox(height: 15),
+                CustomSocialButton(
+                  title: 'Continue With Facebook',
+                  buttonColor: Colors.white,
+                  imagePath: 'assets/images/facebook_logo.png',
+                  onTap: () {
+                    controller.signInWithFacebook();
+                    Get.to(HomeScreen());
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
