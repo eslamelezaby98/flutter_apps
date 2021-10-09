8ac6c2387b4f7a5dafa6dc43f29645ba3000a3e2
Eslam Elezaby <eslamelezaby98@gmail.com>
2021-10-09 15:47:10 +0200

    save value of theme using sharedPref

--- to_do_app/to_do_app/lib/main.dart
+++ to_do_app/to_do_app/lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:to_do_app/screens/home_screen.dart';
 import 'package:to_do_app/services/theme_provider.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(isDarkTheme: false),
-      child: MyApp(),
-    ),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences.getInstance().then(((value) {
+    var isDarkTheme = value.getBool('isDarkTheme') ?? false;
+    runApp(
+      ChangeNotifierProvider(
+        create: (context) => ThemeProvider(isDarkTheme: isDarkTheme),
+        child: MyApp(),
+      ),
+    );
+  }));
 }
 
 class MyApp extends StatelessWidget {
--- to_do_app/to_do_app/lib/services/theme_provider.dart
+++ to_do_app/to_do_app/lib/services/theme_provider.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
+  late SharedPreferences prefs;
+
   // Need a selected theme.
   late ThemeData _selectedTheme;
 
   //Declare a dark and a light theme
-
   ThemeData _light = ThemeData(
     primaryColor: Colors.white,
     scaffoldBackgroundColor: Colors.white,
@@ -34,12 +36,18 @@ class ThemeProvider extends ChangeNotifier {
   ThemeData get getTheme => _selectedTheme;
 
   // Implement a switchTheme method that changes the current theme.
-  void switchTheme() {
+  switchTheme() async {
+    prefs = await SharedPreferences.getInstance();
     if (_selectedTheme == _dark) {
       _selectedTheme = _light;
+      await prefs.setBool('isDarkTheme', false);
     } else {
       _selectedTheme = _dark;
+      await prefs.setBool('isDarkTheme', true);
     }
     notifyListeners();
   }
+
+  // shared_preferences
+
 }
