203c2a5031d2ec1a96474bcab1e7cfffaa1c2acb
Eslam Elezaby <eslamelezaby98@gmail.com>
2021-12-05 14:43:27 +0200

    added app_icon and remove a white screen at start

--- e_commerce_app/lib/presentation/views/on_boarding_view.dart
+++ e_commerce_app/lib/presentation/views/on_boarding_view.dart
@@ -1,12 +1,16 @@
+import 'package:e_commerce_app/presentation/app_manager/color_manager.dart';
 import 'package:flutter/material.dart';
 
 class OnBoradingView extends StatelessWidget {
-  const OnBoradingView({ Key? key }) : super(key: key);
+  const OnBoradingView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      body: const Center(
+        child: Text('Welcome to onBording screen'),
+      ),
     );
   }
 }
--- e_commerce_app/lib/presentation/views/splash_view.dart
+++ e_commerce_app/lib/presentation/views/splash_view.dart
@@ -1,9 +1,39 @@
+import 'dart:async';
+
 import 'package:e_commerce_app/presentation/app_manager/asseet_maanger.dart';
 import 'package:e_commerce_app/presentation/app_manager/color_manager.dart';
+import 'package:e_commerce_app/presentation/app_manager/routes_manager.dart';
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({Key? key}) : super(key: key);
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  Timer? _timer;
+
+  _startDaliy() {
+    _timer = Timer(const Duration(seconds: 2), _goNext);
+  }
+
+  _goNext() {
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startDaliy();
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
