f91c319026cb9276e82168e56c40d342132edd03
Eslam Elezaby <eslamelezaby98@gmail.com>
2021-12-30 06:00:15 +0200

    update key

--- weather_app/lib/helper/constants_manager.dart
+++ weather_app/lib/helper/constants_manager.dart
@@ -1,7 +1,7 @@
 class ConstantsManager {
   //! Api consts
-  //TODO: remove my key
-  static const String myKey = 'f77956a1f962403a81a195329212712';
+  //TODO: put here your key
+  static const String myKey = 'put here your key';
   static const String country = 'Londen';
   static const String baseUrl =
       'http://api.weatherapi.com/v1/forecast.json?key=$myKey&q=$country&days=10&aqi=no&alerts=no';
